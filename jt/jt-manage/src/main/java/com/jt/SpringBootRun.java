b89baf5340b3111de20fa73f6f489277779a8024
刘昱江 <527161772@qq.com>
2021-01-20 19:52:33 +0800

    代码调试2

--- jt/jt-manage/src/main/java/com/jt/SpringBootRun.java
+++ jt/jt-manage/src/main/java/com/jt/SpringBootRun.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.jt.mapper")
 public class SpringBootRun {
-	//测试代码测试正常
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootRun.class, args);
