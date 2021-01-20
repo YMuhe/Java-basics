fc81dfe8af34a3fd0a7a071e825514f7556ec5a6
刘昱江 <527161772@qq.com>
2021-01-20 19:55:35 +0800

    IDEA代码调试3

--- jt/jt-manage/src/main/java/com/jt/SpringBootRun.java
+++ jt/jt-manage/src/main/java/com/jt/SpringBootRun.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.jt.mapper")
 public class SpringBootRun {
-	//IDEA代码调试
+	//IDEA代码调试 GIT调试
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootRun.class, args);
