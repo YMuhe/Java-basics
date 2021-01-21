2cecbe877a4835b7ced332fbdd03f1d9e6f72492
刘昱江 <liulaoshi666@gmail.com>
2021-01-21 14:30:08 +0800

    完成商品分页操作

--- jt/jt-manage/src/main/java/com/jt/mapper/ItemMapper.java
+++ jt/jt-manage/src/main/java/com/jt/mapper/ItemMapper.java
@@ -2,7 +2,14 @@ package com.jt.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jt.pojo.Item;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface ItemMapper extends BaseMapper<Item>{
-	
+    /*Linux系统中严格区分大小写,所以表名及字段都必须与表一致...
+    * 老师为什么我的程序在windows中正常执行 ,linux中报错???
+    * */
+    @Select("SELECT * FROM tb_item ORDER BY updated DESC  LIMIT #{start},#{rows}")
+    List<Item> findItemByPage(int start, Integer rows);
 }
--- jt/jt-manage/src/main/java/com/jt/service/ItemServiceImpl.java
+++ jt/jt-manage/src/main/java/com/jt/service/ItemServiceImpl.java
@@ -16,8 +16,13 @@ public class ItemServiceImpl implements ItemService {
 	private ItemMapper itemMapper;
 
 	/**
-	 * 1.方式1 查询所有的商品信息
+	 * 1.方式1  查询所有的商品信息
 	 * 2.方式2  自己能否手写sql实现分页查询
+	 * 2.1 分页查询规则  sql: select * from tb_item limit  起始位置,记录数
+	 * 		第一页		SELECT * FROM tb_item LIMIT 0,20  [0,19] 共20条
+	 * 		第二页		SELECT * FROM tb_item LIMIT 20,20 [20,39]共20条
+	 * 		第三页		SELECT * FROM tb_item LIMIT 40,20 [40,59]共20条
+	 * 		第N页		SELECT * FROM tb_item LIMIT (page-1)*20,20  共20条
 	 * @param page
 	 * @param rows
 	 * @return
@@ -25,8 +30,10 @@ public class ItemServiceImpl implements ItemService {
 	@Override
 	public EasyUITable findItemByPage(Integer page, Integer rows) {
 
-		//1.查询全部数据   不需要where条件查询是全部
-		List<Item> itemList = itemMapper.selectList(null);
+		//1.手写分页查询
+		//1.1计算起始位置
+		int start = (page-1)*rows;
+		List<Item> itemList = itemMapper.findItemByPage(start,rows);
 		//2.查询总记录数
 		long total = itemMapper.selectCount(null);
 
