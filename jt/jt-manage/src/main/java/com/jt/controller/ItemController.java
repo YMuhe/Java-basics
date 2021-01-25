7fadafcf059c522c478859d68019f869a3668987
刘昱江 <527161772@qq.com>
2021-01-25 11:23:48 +0800

    完成代码删除

--- jt/jt-manage/src/main/java/com/jt/controller/ItemController.java
+++ jt/jt-manage/src/main/java/com/jt/controller/ItemController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.jt.service.ItemService;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,5 +53,43 @@ public class ItemController {
 		}*/
 	 }
 
-	 //添加测试数据AAA
+	/**
+	 * 业务: 实现商品更新操作
+	 * URL:  /item/update
+	 * 参数: 整个form表单   利用对象接收
+	 * 返回值: SysResult对象
+	 */
+	@RequestMapping("update")
+	public SysResult updateItem(Item item){
+
+		itemService.updateItem(item);
+		return SysResult.success();
+	}
+
+	/**
+	 * 业务说明: 实现商品删除
+	 * URL地址:	 /item/delete
+	 * 参数:	 ids: 1474392223,1474392222,1474392220,1474392215
+	 * 返回值:   SysResult对象
+	 * 核心规则: 参数的名称必须与页面中提交的名称一致!!!!
+	 * 			 String request.getParameter("ids");
+	 * 关于SpringMVC参数取值的说明:
+	 * 	1.如果参数与参数之间采用,号的方式进行链接可以自动的将其转化为
+	 * 	数组结构
+	 * 	Long[]  longIds = new Long[strIds.length];
+	 * 		//实现数据类型的转化
+	 * 		for(int i=0;i<strIds.length;i++){
+	 * 			longIds[i] = Long.parseLong(strIds[i]);
+	 *                }
+	 */
+	@RequestMapping("/delete")
+	public SysResult deleteItems(Long... ids){
+
+		itemService.deleteItems(ids);
+		return SysResult.success();
+	}
+
+
+
+
 }
