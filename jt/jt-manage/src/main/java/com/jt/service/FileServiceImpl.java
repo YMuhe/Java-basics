f8b740880408ee6673cd5091cfdfaa2b475604fa
刘昱江 <527161772@qq.com>
2021-01-26 12:07:22 +0800

    完成文件上传4

--- jt/jt-manage/src/main/java/com/jt/service/FileServiceImpl.java
+++ jt/jt-manage/src/main/java/com/jt/service/FileServiceImpl.java
@@ -23,7 +23,6 @@ public class FileServiceImpl implements FileService{
         typeSet.add(".gif");
     }
 
-
     /**
      * 知识点:
      *      1.代码的安全性
@@ -39,18 +38,44 @@ public class FileServiceImpl implements FileService{
      */
     @Override
     public ImageVO upload(MultipartFile uploadFile) {
+        //1.校验文件类型  abc.jpg
+        String fileName = uploadFile.getOriginalFilename().toLowerCase();
+        //1.1 利用正则表达式校验是否满足图片格式要求
+        if(!fileName.matches("^.+\\.(jpg|png|gif)$")){
+            return ImageVO.fail();
+        }
+
+
+        return null;
+    }
+
+
+    /**
+     * 知识点:
+     *      1.代码的安全性
+     *      2.多问一下自己如果什么什么 应该怎么办??
+     *
+     * 文件上传业务说明:
+     * 1.校验文件上传是否为图片类型? jpg|png|gif....
+     * 2.为了防止恶意程序 木马.exe.jpg
+     * 3.为了保证检索速度,需要将图片 分目录存储.
+     * 4.如何防止文件重名???
+     * @param uploadFile
+     * @return
+     */
+   /* @Override
+    public ImageVO upload(MultipartFile uploadFile) {
         //1.校验是否为图片类型
         String fileName = uploadFile.getOriginalFilename();
         //1.1由于大小写问题 导致校验异常bug 所有全部转化为小写
         fileName = fileName.toLowerCase();
         //1.2截取下标
         int index = fileName.lastIndexOf(".");
-        //1.3 如果下标为0表示没有后缀 应该提前结束
-        if(index == 0){
+        //1.3 如果下标为-1表示没有后缀 应该提前结束
+        if(index == -1){
             //没有后缀.程序应该提前结束
             return ImageVO.fail();
         }
-
         //1.4校验是否为图片类型
         String fileType = fileName.substring(index);
         if(!typeSet.contains(fileType)){
@@ -58,5 +83,5 @@ public class FileServiceImpl implements FileService{
             return ImageVO.fail();
         }
             return null;
-    }
+    }*/
 }
