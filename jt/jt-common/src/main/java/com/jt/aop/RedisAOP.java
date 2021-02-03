075b959d8cdd79e47771f10c3c94835d63562a3b
刘昱江 <liulaoshi666@gmail.com>
2021-02-03 11:01:48 +0800

    缓存实现业务2

--- jt/jt-common/src/main/java/com/jt/aop/RedisAOP.java
+++ jt/jt-common/src/main/java/com/jt/aop/RedisAOP.java
@@ -1,16 +1,21 @@
 package com.jt.aop;
 
+import com.jt.anno.CacheFind;
+import com.jt.util.ObjectMapperUtil;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 /*@Service
@@ -20,28 +25,73 @@ import java.util.Arrays;
 @Aspect     //表示我是一个切面
 public class RedisAOP {
 
+    @Autowired
+    private Jedis jedis;
+
     /*
     * 实现AOP业务调用
     * 1.拦截指定的注解
     * 2.利用环绕通知实现
+    * 实现步骤:
+    *       1.获取KEY  必须先获取注解 从注解中获取key?
+    *       2.校验redis中是否有值
+    *     *
+    * 3.知识点补充:
+    *   指定参数名称进行传值,运行期绑定参数类型完成注解的拦截
+    *   joinPoint必须位于参数的第一位.
     */
-    @Around("@annotation(com.jt.anno.CacheFind)")
-    public Object around(ProceedingJoinPoint joinPoint){
-        try {
-            System.out.println("环绕通知执行开始");
-            Object result = joinPoint.proceed();    //1.执行下一个通知 2.执行目标方法
-            System.out.println("环绕通知执行结束");
-            return result;
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-            throw new RuntimeException(throwable);
+    @Around("@annotation(cacheFind)")
+    public Object around(ProceedingJoinPoint joinPoint,CacheFind cacheFind){
+        Object result = null;
+        //key=业务名称::参数
+        String key = cacheFind.key();
+        String args = Arrays.toString(joinPoint.getArgs());
+        key = key + "::" + args;
+
+        //2.校验是否有值
+        if(jedis.exists(key)){
+
+        }else{
+            //redis中没有数据,所以需要查询数据库,将数据保存到缓存中
+            try {
+                result = joinPoint.proceed();
+                String json = ObjectMapperUtil.toJSON(result);
+                //是否设定超时时间
+                if(cacheFind.seconds()>0){
+                    jedis.setex(key, cacheFind.seconds(), json);
+                }else{
+                    jedis.set(key,json);
+                }
+                System.out.println("AOP查询redis缓存");
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
         }
+
+        return result;
     }
 
 
-
-
-
+    /**
+     *   //1.获取key 注解  方法对象  类 方法名称  参数
+     *         Class targetClass = joinPoint.getTarget().getClass();
+     *         //2.获取方法对象
+     *         String methodName = joinPoint.getSignature().getName();
+     *         Object[] args = joinPoint.getArgs();
+     *         Class[] classArgs = new Class[args.length];
+     *         for(int i=0;i<args.length;i++){
+     *             classArgs[i] = args[i].getClass();
+     *         }
+     *         try {
+     *             //反射实例化对象
+     *             Method method = targetClass.getMethod(methodName,classArgs);
+     *             CacheFind cacheFind = method.getAnnotation(CacheFind.class);
+     *             String key = cacheFind.key();
+     *             System.out.println(key);
+     *         } catch (NoSuchMethodException e) {
+     *             e.printStackTrace();
+     *         }
+     */
 
 
 
