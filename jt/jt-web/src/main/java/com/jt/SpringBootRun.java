53b56e28acde440a792a7a7186fc2e75ac8a04d7
刘昱江 <liulaoshi666@gmail.com>
2021-02-05 17:44:45 +0800

    京淘前台项目搭建2

--- jt/jt-web/src/main/java/com/jt/SpringBootRun.java
+++ jt/jt-web/src/main/java/com/jt/SpringBootRun.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+//开箱即用规则 如果需要某些功能,需要添加特定的jar包文件即可
+//前台项目不需要数据源配置
 @SpringBootApplication(exclude=DataSourceAutoConfiguration.class)
 public class SpringBootRun {
 	
