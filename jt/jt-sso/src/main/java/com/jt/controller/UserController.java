8e06e8a67b5bc8f5600d34ab15e5b99c4db8058a
刘昱江 <liulaoshi666@gmail.com>
2021-02-18 11:15:20 +0800

    用户数据校验结构分析

--- jt/jt-sso/src/main/java/com/jt/controller/UserController.java
+++ jt/jt-sso/src/main/java/com/jt/controller/UserController.java
@@ -16,6 +16,20 @@ public class UserController {
     private UserService userService;
 
     /**
+     * 完成用户数据的校验 JT-SSO项目
+     * url地址: http://sso.jt.com/user/check/{param}/{type}
+     * 参数:    param 校验数据/type 校验类型/callback 回调函数
+     * 返回值:  SysResult VO对象     data:true数据存在/false 数据不存在可以使用
+     * 提示:  该请求是JSONP请求方式,,所以需要特定的格式封装...
+     */
+
+
+
+
+
+
+
+    /**
      * 测试: 要求查询user表中的所有的数据
      */
     @RequestMapping("/findAll")
