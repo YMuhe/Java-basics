7acf41739e476a591708eb1b12fa28a1f0377f0c
刘昱江 <liulaoshi666@gmail.com>
2021-02-20 14:38:40 +0800

    完成用户信息回显

--- jt/jt-sso/src/main/java/com/jt/controller/UserController.java
+++ jt/jt-sso/src/main/java/com/jt/controller/UserController.java
@@ -3,11 +3,14 @@ package com.jt.controller;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.jt.pojo.User;
 import com.jt.service.UserService;
+import com.jt.util.ObjectMapperUtil;
 import com.jt.vo.SysResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.JedisCluster;
 
 import java.util.List;
 
@@ -18,6 +21,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private JedisCluster jedisCluster;
 
     /**
      * 完成用户数据的校验 JT-SSO项目
@@ -43,4 +48,34 @@ public class UserController {
 
         return userService.findAll();
     }
+
+    /**
+     * 业务需求:
+     *     根据用户ticket信息,查询用户信息
+     * 1.url地址:http://sso.jt.com/user/query/8d5fc189ccde43f7a6b6bf4aecd9eb0e?callback=jsonp1613793443098&_=1613793443147
+     * 2.请求参数: ticket信息
+     * 3.返回值结果:SysResult对象
+     * 注意: JSONP方式进行跨域请求. callback(JSON)
+     */
+    @RequestMapping("/query/{ticket}")
+    public JSONPObject findUserByTicket(String callback,@PathVariable String ticket){
+
+        //利用ticket从redis中动态获取数据
+        String json = jedisCluster.get(ticket);
+        //User user = ObjectMapperUtil.toObj(json, User.class);
+        if(StringUtils.hasLength(json)){
+            return new JSONPObject(callback, SysResult.success(json));
+        }
+        return new JSONPObject(callback,SysResult.fail());
+    }
+
+
+
+
+
+
+
+
+
+
 }
